eb81100f2c311859971a88658a3f52949c3f5c9b
Yash Kapure <85839788+Yash-kapure25@users.noreply.github.com>
2024-02-11 23:33:45 +0530

    Add files via upload

---
+++ Busbooking/contact_us.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Travel - On - Wheels </title>
+    <link rel="stylesheet" href="cssfile/nav.css">
+    <link rel="stylesheet" href="cssfile/footer_l.css">
+     <link rel="stylesheet" href="cssfile/contact_us.css">
+  <!--  <link rel="stylesheet" type="text/css" href="cssfile/container.css">-->
+   
+    <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+   <!-- <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">-->
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+
+    <style type="text/css">
+
+     
+       body{
+       padding: 0;
+       margin: 0;
+       line-height: 1.5;
+       box-sizing: border-box;
+       color:rgba(248, 248, 248, 0.938);
+       background-image: url(image/7.jpg);
+       background-size: cover;
+       background-repeat: no-repeat;
+       background-attachment: fixed;
+ 
+      }
+      
+    </style>
+
+  </head>
+  <body>
+
+
+
+  
+
+
+              <!--this is the header callling(nav bar)-->
+
+            <?php include("nav.php");
+             ?>
+
+
+            <section id="fancy-form">
+   <div class="container">
+    <div class="form-sections">
+      <!-- Form left -->
+      <div class="Form-left">
+        <h1>Get In Touch</h1>
+        <div class="line"></div> <!--border-bottom line-->
+        <p style="color:grey;">Contact us for latest news and updates. subscribe our news letter :)</p><br>
+
+        <!--first Heading -->
+        <h4>ADDRESS</h4>
+         <span>Dbatu, Lonere-Raigad, Maharashtra </span>
+         <hr><br><br>
+
+         <!--second Heading -->
+        <h4>PHONE</h4>
+         <span>(+91)9595468303</span>
+         <hr><br><br>
+
+       <!--third Heading -->
+        <h4>EMAIL</h4>
+         <span>contactus@travelonwheels.com</span>
+         <hr> <br>
+
+          social media icons 
+          <a href="www.facebook.com" class="fab fa-facebook"></a>
+          <a href="www.twiiter.com" class="fab fa-twitter"></a>
+          <a href="www.google.com" class="fab fa-google"></a>
+          <a href="https://www.linkedin.com/in/yash-kapure-9bb18a285/" class="fab fa-linkedin"></a>
+          <a href="https://github.com/Yash-kapure25" class="fab fa-github"></a>
+      </div>
+
+      <!-- form right -->
+      <div class="Form-right">
+        <h1>Contact Us</h1>
+        <div class="line"></div>
+        <!-- form -->
+        <form action="" method="post">
+          <h5>NAME</h5>
+          <input type="text"><br><br>
+          <h5>EMAIL</h5>
+          <input type="email"><br><br>
+          <h5>PHONE</h5>
+          <input type="number"><br><br>
+          <h5>YOUR MESSAGE</h5>
+          <textarea name="" id="" cols="50" rows="7"></textarea><br>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+    </div>
+  </section>
+
+             
+
+
+  </body>
+</html>
---
+++ Busbooking/footer.php
@@ -0,0 +1,74 @@
+<footer>
+        <div class="footerInfo">
+            <div class="footerContainer">
+                <div class="footerItem">
+
+                    <div class="socialInfo">
+                        <div class="mainTitle">
+                            <h4> Follow Us <span></span></h4>
+                        </div>
+                        <div class="socialItem">
+                            <div class="socialLink facebook">
+                                <i href="www.facebook.com" class="fab fa-facebook-f"></i>
+                            </div>
+                            <div class="socialLink instagram">
+                                <i href="www.instagram.com" class="fab fa-instagram"></i>
+                            </div>
+                            <div class="socialLink twitter">
+                                <i href="www.twitter.com" class="fab fa-twitter"></i>
+                            </div>
+                            <div class="socialLink google">
+                                <i href="www.google.com" class="fab fa-google"></i>
+                            </div>
+                            <div class="socialLink linkedin"> 
+                                <i href="https://www.linkedin.com/in/yash-kapure-9bb18a285/" class="fab fa-linkedin"></i>
+                            </div>
+                             <div class="socialLink github">
+                                <i href="https://github.com/Yash-kapure25" class="fab fa-github"></i>
+                            </div>
+                        </div>
+                    </div>
+<!--  -->
+                    <div class="socialInfo">
+                        <div class="mainTitle">
+                            <h4> Service's <span></span></h4>
+                        </div>
+                        <div class="socialItem">
+                            <a href="home.php"> Home </a>
+                            <a href="aboutus.php"> About Us </a>
+                            <a href="help.php"> Services</a>
+                            <a href="loginMenu.php"> Quick Booking </a>
+                        </div>
+                    </div>
+<!--
+                    <div class="socialInfo">
+                        <div class="mainTitle">
+                            <h4> Our Health <span></span></h4>
+                        </div>
+                        <div class="socialItem">
+                            <a href="#"> Emergency dental care </a>
+                            <a href="#"> check up</a>
+                            <a href="#"> treatment of personal diseases </a>
+                            <a href="#"> tooth extraction </a>
+                        </div>
+                    </div>  -->
+                    <div class="socialInfo">
+                        <div class="mainTitle">
+                            <h4> Address <span></span></h4>
+                        </div>
+                        <h5 class="addressTitle">Lonere - Maharashtra, India </h5>
+                        <div class="callInfo">
+                            <h6>Call</h6> <i class="fas fa-arrow-right"></i>
+                            <span>+91 8669207321</span>
+                        </div>
+                        <div class="footerInfo">
+                            <h4>Made by TravelonWheels Team  &trade; &copy; <br>
+                            2019-2020 All Rights are reserved</h4>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+
